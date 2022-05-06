37c9721557c81f1c49521300230373ed7cf8aa3a
Falko Andrii <andrey2000fal@gmail.com>
2022-05-06 17:53:04 +0300

    first commit

---
+++ build/CMakeFiles/useable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/useable.dir/useable.c.o"
+  "useable"
+  "useable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/useable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/C++/mp2020/tasks/Simple programs
+# Build directory: /mnt/e/C++/mp2020/tasks/Simple programs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
