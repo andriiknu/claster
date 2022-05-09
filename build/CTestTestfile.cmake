95f3aa28d14261d25dc7de9f33fe8249f6d8a7ca
andriiknu <91428670+andriiknu@users.noreply.github.com>
2022-05-09 11:27:26 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/C++/mp2020/tasks/Simple programs
-# Build directory: /mnt/e/C++/mp2020/tasks/Simple programs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
